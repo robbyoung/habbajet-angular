e3d4f314c1fb81bd6db462117b8f52dfcc29a37a
Robert Young <robertyoung10101@gmail.com>
2018-08-25 09:59:21 +1200

    Staged files from deletion branch

---
+++ app/views/habbajet-tab-view/empty-tab/empty-tab.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "empty-tab",
+    templateUrl: "views/habbajet-tab-view/empty-tab/empty-tab.html",
+    styleUrls: ["views/habbajet-tab-view/empty-tab/empty-tab.css"]
+})
+
+export class EmptyTabComponent {
+    
+    constructor() {}
+
+    ngOnInit() {}
+}
